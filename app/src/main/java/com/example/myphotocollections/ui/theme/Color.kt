2263767018baf82e1c4fa6eef714af700aac52a0
furkantzn <furkantuzun96@gmail.com>
2024-10-29 12:52:42 +0300

    Theme has been changed.

---
+++ app/src/main/java/com/example/myphotocollections/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.myphotocollections.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Light Color scheme
+val PrimaryColor = Color(0xFF2C394B)
+val PrimaryText = Color(0xFFFF4C29)
+val SecondaryText = Color(0xFF334756)
+val Background = Color(0xFF082032)
+val Black = Color(0xFF212121)
+val White = Color(0xFFF5F5F5)
+val BackgroundTransparent = Color(0x9A082032)
+
+val DarkThemePrimaryColor = Color(0xFF2C394B)
+val DarkThemePrimaryText =Color(0xFFFF4C29)
+val DarkThemeSecondaryText = Color(0xFF334756)
+val DarkThemeBackground = Color(0xFF082032)
--- app/src/main/java/com/example/myphotocollections/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myphotocollections/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.myphotocollections.ui.activities.ui.theme
+package com.example.myphotocollections.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -16,25 +16,21 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkThemePrimaryColor,
+    onPrimary = DarkThemePrimaryText,
+    background = DarkThemeBackground,
+    onBackground = BackgroundTransparent,
+    secondary = DarkThemeSecondaryText,
+    primaryContainer = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryColor,
+    onPrimary = PrimaryText,
+    background = Background,
+    onBackground = BackgroundTransparent,
+    secondary = SecondaryText,
+    primaryContainer = White
 )
 
 @Composable
