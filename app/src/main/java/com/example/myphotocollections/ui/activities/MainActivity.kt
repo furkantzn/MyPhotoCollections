e0ff50e99239251fc06088684d5adb32aed75522
furkantzn <furkantuzun96@gmail.com>
2024-11-03 12:43:44 +0300

    Modified splash screen.

--- app/src/main/java/com/example/myphotocollections/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/myphotocollections/ui/activities/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.myphotocollections.ui.activities
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.myphotocollections.data.services.SharedPrefManager
 import com.example.myphotocollections.ui.navigation.SetUpNavHost
 import com.example.myphotocollections.ui.theme.MyPhotoCollectionsTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
